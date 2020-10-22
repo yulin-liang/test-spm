5bed9f91c79f9f2ff0ba158b338a0f9c3ec17baf
yulin-liang <yliangsysu@gmail.com>
2020-10-22 13:33:00 -0700

    t

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     .target(
       name: "spm-demo",
       dependencies: [
-        .product(name: "gRPC-Core", package: "gRPC"),
+        .product(name: "gRPC-cpp", package: "gRPC"),
       ],
       path: ".",
       exclude: [
